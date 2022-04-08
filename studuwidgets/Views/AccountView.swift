6be876b84ccac31b8d4bc5b4958987c48576d9cc
Rostislav Brož <rostislav.broz@outlook.com>
2022-04-08 19:44:29 +0200

    Create `AccountView`

---
+++ studuwidgets/Views/AccountView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountView.swift
+//  studuwidgets
+//
+//  Created by Rostislav Brož on 4/8/22.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
