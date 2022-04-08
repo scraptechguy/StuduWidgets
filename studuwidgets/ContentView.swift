4e7ca1f15ad8b732da41c153fa848026f289f7be
Rostislav Brož <rostislav.broz@outlook.com>
2022-04-08 19:07:04 +0200

    Initial Commit

---
+++ studuwidgets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  studuwidgets
+//
+//  Created by Rostislav Brož on 4/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ studuwidgets/studuwidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  studuwidgetsApp.swift
+//  studuwidgets
+//
+//  Created by Rostislav Brož on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct studuwidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
