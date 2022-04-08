bad8b32a85cdeb349873de10013a2c4425dacc07
Rostislav Brož <rostislav.broz@outlook.com>
2022-04-08 19:23:23 +0200

    Create initial file structure

---
+++ studuwidgets/ViewModels/ContentModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentModel.swift
+//  studuwidgets
+//
+//  Created by Rostislav Brož on 4/8/22.
+//
+
+import Foundation
+import SwiftUI
+
+class ContentModel: ObservableObject {
+    
+}
--- studuwidgets/studuwidgetsApp.swift
+++ studuwidgets/studuwidgetsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct studuwidgetsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
