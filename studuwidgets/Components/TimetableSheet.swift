8e879589f8fb73b0bf1d71bb44602713cc643f7f
Rostislav Brož <rostislav.broz@outlook.com>
2022-04-09 10:43:17 +0200

    Create files for sheets

---
+++ studuwidgets/Components/TimetableSheet.swift
@@ -0,0 +1,20 @@
+//
+//  TimetableSheet.swift
+//  studuwidgets
+//
+//  Created by Rostislav Brož on 4/9/22.
+//
+
+import SwiftUI
+
+struct TimetableSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimetableSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        TimetableSheet()
+    }
+}
